6a066c3d54145dba7bf4ff640b5099f9c5edcd38
Christopher Vasquez <Christopher99@users.noreply.github.com>
2026-07-03 03:34:20 -0500

    Feat: Agregar endpoint Crear Clave Firma

--- application/src/main/java/com/hexa_system/controller/AuthController.java
+++ application/src/main/java/com/hexa_system/controller/AuthController.java
@@ -4,9 +4,14 @@ import com.hexa_system.aggregates.dto.SignInRequest;
 import com.hexa_system.aggregates.dto.SignInResponse;
 import com.hexa_system.aggregates.dto.VerificacionDTO;
 import com.hexa_system.ports.in.AuthServiceIn;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.Key;
+import java.util.Base64;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -25,9 +30,17 @@ public class AuthController {
     public ResponseEntity<SignInResponse> verificarUsuario(@RequestBody VerificacionDTO dto) {
         return ResponseEntity.ok(authServiceIn.verificarCodigoIn(dto));
     }
+
     @PostMapping("/refresh")
     public ResponseEntity<SignInResponse> refresh(
-            @RequestParam String refreshToken) {
-        return ResponseEntity.ok(authServiceIn.refreshTokenIn(refreshToken));
+            @RequestHeader("refreshToken") String auth) {
+        return ResponseEntity.ok(authServiceIn.refreshTokenIn(auth));
+    }
+
+    @GetMapping("/clave")
+    public ResponseEntity<String> getClave(){
+        Key key= Keys.secretKeyFor(SignatureAlgorithm.HS512);
+        String clave= Base64.getEncoder().encodeToString(key.getEncoded());
+        return ResponseEntity.ok(clave);
     }
 }
